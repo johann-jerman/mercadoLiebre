9f026d01978de16f6804b77e52b238d56904bb55
unknown <johannjerman014@gmail.com>
2023-01-10 18:54:26 -0300

    Mercado liebre

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+});
+app.get('/register', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+});
+app.get('/login', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+});
+
+app.listen(PORT);
